0b4d0a43835df94809e3d77105cb51636ed70ec7
Laxman Singh <singhlaxman2397@gmail.com>
2025-09-20 14:57:19 +0530

    Login page signup page done

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
 ]
+
--- main/views.py
+++ main/views.py
@@ -1,6 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
-
 def home(request):
     return render(request, 'home_page/index.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        login_input = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user_obj = User.objects.get(email=login_input)
+            username = user_obj.username
+        except ObjectDoesNotExist:
+            username = login_input
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+        messages.error(request, 'Invalid credentials')
+
+    return render(request, 'login_page/login.html')
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        username = request.POST.get('username')
+
+        if not username:
+            if email:
+                username = email.split('@')[0]  
+            else:
+                return f"user_{User.objects.count()+1}"
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'User with this username already exists')
+            return render(request, 'signup_page/signup.html')
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+            first_name=first_name,
+            last_name=last_name
+        )
+        login(request, user)
+    return render(request, 'signup_page/signup.html')
